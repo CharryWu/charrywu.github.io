950f86be92fc7f5be933a825f134755ed96ca4d0
Charry Wu <wuzc.charry2013@gmail.com>
2025-02-25 20:10:57 -0800

    deploy

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,5 @@
 yarn docs:build # navigate into the build output directory
-cd .\docs\.vitepress\dist # if you are deploying to a custom domain
+Set-Location .\docs\.vitepress\dist # if you are deploying to a custom domain
 
 # echo 'www.example.com' > CNAME git init
 git add -A
